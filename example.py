58b389c723947687b05d340b63e48ef71a0a987d
John Fike <johnfike223@gmail.com>
2024-02-29 15:44:56 -0700

    update pinhole()
    remove showing images (binary image with dot, etc)
    
    return deviation from middle of image.
    
    positive deviation means towards lower right corner of image
    
    negative deviation means towards upper left corner of image

--- example.py
+++ example.py
@@ -7,9 +7,9 @@ import datetime
 # when I run the yolo model, it creates a text file for every image that has the coordinates to the colonies it has found, and places it in 
 # ./runs/detect/predict/labels this is what my code grabs and uses to do discimination for crowded colonies, and is why we have to deal with text files
 # this is what is getting moved to yolo_dump
-cpr.process_petri_dish_image(image_folder_path='./baseplatePhotos/', good_colony_coord_output_path='./good_colony_coords/', raw_yolo_dump_path='./yolo_dump/')
+# cpr.process_petri_dish_image(image_folder_path='./baseplatePhotos/', good_colony_coord_output_path='./good_colony_coords/', raw_yolo_dump_path='./yolo_dump/')
 
-cpr.create_metadata(image_folder_path='./baseplatePhotos/', colony_coords_folder_path='./sampleColonies/', create_petri_dish_view=True, create_colony_view= True)
+# cpr.create_metadata(image_folder_path='./baseplatePhotos/', colony_coords_folder_path='./sampleColonies/', create_petri_dish_view=True, create_colony_view= True)
 
-cpr.pinhole('./pinhole_test_images/pinhole_lights_on.jpg', save_image_path= './pinhole_test_images/pinhole_test.jpg', row_deviation_threshold=.1, column_deviation_threshold=.1, center_point=(0.5, 0.48))
+print(cpr.pinhole('./pinhole_test_images/pinhole_lights_on.jpg', save_image_path= './pinhole_test_images/pinhole_test.jpg', row_deviation_threshold=.1, column_deviation_threshold=.1, center_point=(0.5, 0.48)))
 
